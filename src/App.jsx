2c349e0978c9aae1eab7fc925bd5104df44c81f7
Claws35 <mateocuti@gmail.com>
2025-05-30 18:43:45 -0300

    Update App component layout and modify carteles data structure

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,15 @@ import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 function App() {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-[#f6f2e6] overflow-hidden relative ">
-      <h1 className="text-6xl font-semibold mb-8 z-10 font-[new-order]">
+      <h1 className="text-5xl sm:text-6xl font-semibold mt-20 sm:mt-10 z-10 font-[new-order] text-center">
         Mi camino creativo
       </h1>
 
-      <div className="w-full h-[60vh] relative mt-10 z-0">
+      <div className="w-full  sm:h-[60vh] relative mt-10 z-0">
         {/* Swiper container */}
         <Swiper
           effect={"coverflow"}
+          initialSlide={18}
           grabCursor={true}
           centeredSlides={true}
           slidesPerView={"auto"}
@@ -34,18 +35,25 @@ function App() {
             modifier: 1,
             slideShadows: false,
           }}
-          navigation={true}
           modules={[EffectCoverflow, Pagination, Navigation]}
           className="w-full h-full"
         >
           {/* Primera diapositiva especial */}
           <SwiperSlide className="!w-[300px] !h-[380px] relative">
             <img
-              src={imagenPj}
-              alt="Imagen inicial"
-              className="h-[180px] object-cover absolute bottom-[10px] right-[42%] z-20 "
+              className=" absolute bottom-7 right-30 h-40"
+              src={imagenFantasma}
+              alt=""
             />
-            <div className="absolute bottom-[10px] left-79 transform -translate-x-1/2 w-[120%] h-4 bg-[#f1ae3f] "></div>
+          </SwiperSlide>
+
+          <SwiperSlide className="!w-[300px] !h-[380px] relative"></SwiperSlide>
+          <SwiperSlide className="!w-[300px] !h-[380px] relative"></SwiperSlide>
+          <SwiperSlide className="!w-[300px] !h-[380px] relative">
+            <h3 className=" absolute text-2xl bottom-7">
+              Aqui es donde comenzó...
+            </h3>
+            <div className="absolute bottom-[10px] left-1/2 transform -translate-x-1/2 w-[120%] h-4 bg-[#f1ae3f] "></div>
           </SwiperSlide>
 
           {/* Map de carteles */}
@@ -63,23 +71,16 @@ function App() {
               <div className="absolute bottom-[10px] left-1/2 transform -translate-x-1/2 w-[120%] h-4 bg-[#f1ae3f] "></div>
             </SwiperSlide>
           ))}
-
-          {/* Última diapositiva especial */}
-          <SwiperSlide className="!w-[300px] !h-[380px] relative">
-            <h3 className=" absolute text-2xl bottom-7">
-              Aqui es donde comenzó...
-            </h3>
-            <div className="absolute bottom-[10px] left-1/2 transform -translate-x-1/2 w-[120%] h-4 bg-[#f1ae3f] "></div>
-          </SwiperSlide>
-          <SwiperSlide className="!w-[300px] !h-[380px] relative"></SwiperSlide>
-          <SwiperSlide className="!w-[300px] !h-[380px] relative"></SwiperSlide>
           <SwiperSlide className="!w-[300px] !h-[380px] relative">
             <img
-              className=" absolute bottom-7 right-30 h-40"
-              src={imagenFantasma}
-              alt=""
+              src={imagenPj}
+              alt="Imagen inicial"
+              className="h-[180px] object-cover absolute bottom-[10px] right-[42%] z-20 "
             />
+            <div className="absolute bottom-[10px] left-0 transform -translate-x-1/2 w-[100%] h-4 bg-[#f1ae3f] "></div>
           </SwiperSlide>
+
+          {/* Última diapositiva especial */}
         </Swiper>
       </div>
     </div>
